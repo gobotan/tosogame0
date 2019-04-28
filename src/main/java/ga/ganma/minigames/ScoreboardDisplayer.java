9c0f488d6708be64a17eaaf4c67bdb9f18b1f2e1
siloneco <siloneco4646@gmail.com>
2019-04-28 13:02:41 +0900

    [Fix] 文法修正

--- src/main/java/ga/ganma/minigames/ScoreboardDisplayer.java
+++ src/main/java/ga/ganma/minigames/ScoreboardDisplayer.java
@@ -48,7 +48,7 @@ public class ScoreboardDisplayer {
 			obj.getScore(StringUtils.repeat("-", 20)).setScore(0);
 			obj.getScore("  ").setScore(-1);
 			obj.getScore(ChatColor.AQUA + "create by ganma_").setScore(-2);
-			obj.getScore(ChatColor.GOLD + "play the mc.zosukue.com").setScore(-3);
+			obj.getScore(ChatColor.GOLD + "play " + ChatColor.RED + "mc.zosukue.com").setScore(-3);
 
 			p.setScoreboard(board);
 		}
